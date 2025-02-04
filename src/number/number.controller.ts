dd9982213d8d1ede5e756f2b3bebaab6b0e1f00c
Osarugue Enehizena <nehirugue@gmail.com>
2025-02-04 02:13:41 +0100

    fix: add validation for missing number query parameter in classifyNumber endpoint

--- src/number/number.controller.ts
+++ src/number/number.controller.ts
@@ -4,8 +4,21 @@ import { NumberService } from './number.service';
 @Controller('api')
 export class NumberController {
   constructor(private readonly numberService: NumberService) {}
+
   @Get('classify-number')
-  async classifyNumber(@Query('number') numberString: string) {
+  async classifyNumber(@Query('number') numberString?: string) {
+    if (!numberString) {
+      throw new BadRequestException({
+        detail: [
+          {
+            type: 'missing',
+            loc: ['query', 'number'],
+            msg: 'Field required',
+            input: null,
+          },
+        ],
+      });
+    }
     const number = parseInt(numberString, 10);
     if (isNaN(number)) {
       throw new BadRequestException({
